00fff693808dc537f95741c67cd98a8eafb8dd35
Yoncity <yonatan.dawit.yd2@gmail.com>
2019-09-26 22:42:08 +0300

    Added Post Feature

--- src/components/main/create_post/create_post.jsx
+++ src/components/main/create_post/create_post.jsx
@@ -1,36 +1,92 @@
 import React, { Component } from "react";
 
-class CreatePost extends Component {
-  getPostInput(e) {
-    const { name, value } = e.target;
-    if (name === "title") {
-    } else if (name === "post_data") {
-    }
-  }
+import "./create_post.css";
 
-  postArticle = () => {};
+// Redux Related
+import { connect } from "react-redux";
+import {
+  addPostBegin,
+  addPostSuccess,
+  addPostError,
+  addPostFields,
+  clearPostFields
+} from "../../../action/postAction";
+
+class CreatePost extends Component {
+  getPostInput = e => {
+    const { name, value } = e.target;
+    const tmp = this.props.posts;
+
+    if (name === "title") tmp["title"] = value;
+    else if (name === "post_data") tmp["body"] = value;
+
+    this.props.addPostFields(tmp);
+  };
+
+  postArticle = () => {
+    const { title, body } = this.props.post_fields;
+    console.log(title);
+    let tmp = this.props.posts;
+    if (title && body) {
+      try {
+        this.props.addPostBegin();
+        tmp.push({ title, body });
+        this.props.addPostSuccess(tmp);
+        this.props.clearPostFields();
+        console.log(tmp);
+      } catch (error) {
+        this.props.addPostError(error);
+      }
+    }
+  };
 
   render() {
     return (
-      <div>
-        <form>
+      <div id="create_post">
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+          }}
+        >
           <input
             type="text"
             name="title"
             onChange={this.getPostInput}
             placeholder="Title"
           />
-          <input
-            type="text"
-            name="post_data"
-            onChange={this.getPostInput}
-            placeholder="Message"
-          />
-          <input type="submit" onClick={this.postArticle} value="POST" />
+
+          <div id="fields_container">
+            <textarea
+              id="body_field"
+              type="textarea"
+              name="post_data"
+              onChange={this.getPostInput}
+              placeholder="Write Article"
+            />
+            <input type="submit" onClick={this.postArticle} value="POST" />
+          </div>
         </form>
       </div>
     );
   }
 }
 
-export default CreatePost;
+const mapState = state => ({
+  posts: state.posts,
+  post_fields: state.post_fields,
+  loading: state.loading,
+  error: state.error
+});
+
+const mapDispatch = {
+  addPostBegin,
+  addPostSuccess,
+  addPostError,
+  addPostFields,
+  clearPostFields
+};
+
+export default connect(
+  mapState,
+  mapDispatch
+)(CreatePost);
--- src/reducer/postReducer.js
+++ src/reducer/postReducer.js
@@ -1,15 +1,37 @@
 import {
   FETCH_POST_BEGIN,
   FETCH_POST_SUCCESS,
-  FETCH_POST_ERROR
+  FETCH_POST_ERROR,
+  ADD_POST_BEGIN,
+  ADD_POST_SUCCESS,
+  ADD_POST_ERROR,
+  ADD_POST_FIELDS,
+  CLEAR_POST_FIELDS
 } from "../action/postAction";
 
 const initialState = {
-  posts: []
+  posts: [],
+  post_fields: {
+    title: null,
+    body: null
+  }
 };
 
 export default function postReducer(state = initialState, action) {
   switch (action.type) {
+    case ADD_POST_FIELDS:
+      return {
+        ...state,
+        post_fields: action.payload.fields
+      };
+    case CLEAR_POST_FIELDS:
+      return {
+        ...state,
+        post_fields: {
+          title: null,
+          body: null
+        }
+      };
     case FETCH_POST_BEGIN:
       return { ...state, loading: true };
 
@@ -21,6 +43,23 @@ export default function postReducer(state = initialState, action) {
       };
 
     case FETCH_POST_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.res
+      };
+
+    case ADD_POST_BEGIN:
+      return { ...state, loading: true };
+
+    case ADD_POST_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        posts: action.payload.posts
+      };
+
+    case ADD_POST_ERROR:
       return {
         ...state,
         loading: false,
